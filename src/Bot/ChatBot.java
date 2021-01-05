d44a5e2a1f2bdbca6b0a5e8cbac07928350d40ae
AndreSoares94 <andre.soares94@hotmail.com>
2021-01-05 16:33:59 +0000

    aiml

--- src/Bot/ChatBot.java
+++ src/Bot/ChatBot.java
@@ -1,21 +1,31 @@
 package Bot;
 
+import org.alicebot.ab.*;
+import org.alicebot.ab.Chat;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.ClassNotFoundException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.stream.DoubleStream;
 
 public class ChatBot {
     //static ServerSocket variable
     private static ServerSocket server;
     //socket server port on which it will listen
     private static int port = 9876;
+    private static String botname;
 
     public static void main(String args[]) throws IOException, ClassNotFoundException{
         //create the socket server object
         server = new ServerSocket(port);
+        botname = "mybot";
+
+        String path = "src/Bot";
+        Bot bot = new Bot(botname, path);
+        Chat chatSession = new Chat(bot);
+
         //keep listens indefinitely until receives 'exit' call or program terminates
         while(true){
             System.out.println("Waiting for the user message");
@@ -25,11 +35,12 @@ public class ChatBot {
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             //convert ObjectInputStream object to String
             String message = (String) ois.readObject();
+            String answer = chatSession.multisentenceRespond(message);
             System.out.println("Message Received: " + message);
             //create ObjectOutputStream object
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             //write object to Socket
-            oos.writeObject("Hello, you said: "+message);
+            oos.writeObject("Answer: "+answer);
             //close resources
             ois.close();
             oos.close();
